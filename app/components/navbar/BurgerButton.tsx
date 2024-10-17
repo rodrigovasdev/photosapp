741a943f1076e238d96a46e396b07492eb05e051
Rodrigo <rorro19vas@gmail.com>
2024-10-17 01:25:37 -0300

    Ajustes Menu Hamburguesa

--- app/components/navbar/BurgerButton.tsx
+++ app/components/navbar/BurgerButton.tsx
@@ -10,21 +10,21 @@ export default function BurgerButton(){
         <div className='absolute top-0 right-0 sm:collapse'>
                   <Disclosure as="div">              
                     <DisclosureButton className='absolute top-0 right-0' as="button"><Bars3Icon className='w-[4rem]'></Bars3Icon></DisclosureButton>
-                    <DisclosurePanel className='mt-20 bg-stone-700/75  rounded-lg divide-y-2 divide-gray-300/75 mr-1 ' as="ul">                     
-                        <li className={`${roboto.className}  mb-1.5 p-1 pl-5 text-white ${
-                            pathname === '/'? 'text-orange-300' : ''}
+                    <DisclosurePanel className='mt-20  text-left bg-stone-700/75  rounded-lg divide-y-2 divide-gray-300/75 mr-1 ' as="ul">                     
+                        <li className={`${roboto.className} pl-5 mb-1  text-white ${
+                            pathname === '/'? 'text-amber-400' : ''}
                             }`}><Link href='/'>
                                   Fotos
                                 </Link>
                         </li>
-                        <li className={`${roboto.className} mb-1.5 p-1 pl-5 text-white ${
-                            pathname === '/about'? 'text-orange-300' : ''}
+                        <li className={`${roboto.className} pl-5 mb-1  text-white ${
+                            pathname === '/about'? 'text-amber-400' : ''}
                             }`}><Link href='/about'>
                                   Acerca
                                 </Link>
                         </li>
-                        <li className={`${roboto.className}  mb-1.5 p-3 pl-5  text-white ${
-                            pathname === '/contact'? 'text-orange-300' : ''}
+                        <li className={`${roboto.className} pl-5 pr-3 pb-1  text-white ${
+                            pathname === '/contact'? 'text-amber-400' : ''}
                             }`}><Link href='/contact'>
                                   Contacto
                                 </Link>
