6c0485cc7a06f79e6a4cb60ddbedb1e6d37069c8
Rodrigo <rorro19vas@gmail.com>
2024-10-18 01:24:18 -0300

    Correccion mobile navbar

--- app/components/navbar/BurgerButton.tsx
+++ app/components/navbar/BurgerButton.tsx
@@ -11,19 +11,19 @@ export default function BurgerButton(){
                   <Disclosure as="div">              
                     <DisclosureButton className='absolute top-0 right-0' as="button"><Bars3Icon className='w-[4rem]'></Bars3Icon></DisclosureButton>
                     <DisclosurePanel className='mt-20  text-left bg-stone-700/75  rounded-lg divide-y-2 divide-gray-300/75 mr-1 ' as="ul">                     
-                        <li className={`${roboto.className} pl-5 mb-1  text-white ${
+                        <li className={`${roboto.className} pl-5 mb-1 p-1  text-white ${
                             pathname === '/'? 'text-amber-400' : ''}
                             }`}><Link href='/'>
                                   Fotos
                                 </Link>
                         </li>
-                        <li className={`${roboto.className} pl-5 mb-1  text-white ${
+                        <li className={`${roboto.className} pl-5 mb-1 p-1  text-white ${
                             pathname === '/about'? 'text-amber-400' : ''}
                             }`}><Link href='/about'>
                                   Acerca
                                 </Link>
                         </li>
-                        <li className={`${roboto.className} pl-5 pr-3 pb-1  text-white ${
+                        <li className={`${roboto.className} pl-5 pr-3 pb-2 p-1  text-white ${
                             pathname === '/contact'? 'text-amber-400' : ''}
                             }`}><Link href='/contact'>
                                   Contacto
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
 
   return (
       <header className="fixed top-0 left-0 z-50  w-full bg-stone-700/90 border-stone-700 border-b-stone-700/75 ">
-        <nav aria-label="Global" className="h-16 mx-auto flex max-w-7xl items-center justify-around p-6 lg:px-8">
+        <nav aria-label="Global" className="h-16 mx-auto flex max-w-7xl items-center justify-between sm:justify-around p-6 lg:px-8">
           <div className='text-white text-4xl'>
             <Link href="/">
               <p className={caveat.className}>PHOTOSGRAM</p>
