576eeee1bb1261a9a743a4ac9bf70914151716ba
rodrigovasdev <146775218+rodrigovasdev@users.noreply.github.com>
2025-09-10 05:08:18 -0300

    Increase top padding in PostListContent for improved layout

--- app/components/home/PostListContent.tsx
+++ app/components/home/PostListContent.tsx
@@ -6,7 +6,7 @@ export default async function PostListContent() {
   const posts = await getPosts();
 
   return (
-    <div className="grid pt-28 bg-neutral-300 grid-cols-1 pb-8 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid pt-40 bg-neutral-300 grid-cols-1 pb-8 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {posts?.albums?.map((post: Album) => (
         <PostPreview key={post.id} post={post} />
       ))}
